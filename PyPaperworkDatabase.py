a0d493df30a3c2ebaec844847ba99366896fb205
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-09-15 14:13:46 -0500

    ConnectionCodeMadeWithTestConnection

--- PyPaperworkDatabase.py
+++ PyPaperworkDatabase.py
@@ -1,9 +1,29 @@
 #PyPaperworkDatabase
 import os
-import odbc
+import pyodbc
 
+'''Starting with connection to database using pyodbc. Then modifiing the database.'''
+connStr = (
+    r"DRIVER = {Microsoft Access Driver (*.mdb, *.accdb)};"
+    r"DBQ=F:\ScannedRandomPaperwork\A_ScannedPaperworkInThisFile.accdb;"
+)
+conn = pyodbc.connect(connStr)
+# Execute a SELECT statement
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM your_table")
+
+# Fetch all rows
+rows = cursor.fetchall()
+
+# Print the rows
+for row in rows:
+    print(row)
+
+
+'''Modification of database'''
 filesInFolder = []
 firstBreak = ""
+
 
 def copy_file_names(folder_path):
     for fileName in os.listdir(folder_path):
