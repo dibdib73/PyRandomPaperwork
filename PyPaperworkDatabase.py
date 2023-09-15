f6be66926178bd9c1ed31e1c17cb871922af054d
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-09-15 12:53:37 -0500

    AddedListForFilesInFolder

--- PyPaperworkDatabase.py
+++ PyPaperworkDatabase.py
@@ -1,8 +1,12 @@
 #PyPaperworkDatabase
 import os
 
+filesInFolder = []
+
 def copy_file_names(folder_path):
     for fileName in os.listdir(folder_path):
-        print(fileName)
+        filesInFolder.append(fileName)
 
-copy_file_names(r'"F:\ScannedRandomPaperwork"')
+
+copy_file_names(r'F:\ScannedRandomPaperwork')
+print(filesInFolder)
