afc67eb4b52cecaba56f0e3688547d9d882ec303
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-09-15 13:43:33 -0500

    installed odbc with pip

--- PyPaperworkDatabase.py
+++ PyPaperworkDatabase.py
@@ -1,12 +1,24 @@
 #PyPaperworkDatabase
 import os
+import odbc
 
 filesInFolder = []
+firstBreak = ""
 
 def copy_file_names(folder_path):
     for fileName in os.listdir(folder_path):
         filesInFolder.append(fileName)
+    return filesInFolder
+
+def first_split(firstBreak):
+    stripped = firstBreak.split('.')
+    stripped = stripped[0]
+    stripped = stripped.split('_')
+    return stripped
 
 
-copy_file_names(r'F:\ScannedRandomPaperwork')
-print(filesInFolder)
+
+filesInFolder = copy_file_names(r'F:\ScannedRandomPaperwork')
+strippedTest = first_split(filesInFolder[1])
+
+print(strippedTest)
