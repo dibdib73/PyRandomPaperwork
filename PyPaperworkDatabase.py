6efa899568e1739f552a27defdd6cc176344dd16
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-09-24 14:50:29 -0500

    ChangedAddContentFunction

--- PyPaperworkDatabase.py
+++ PyPaperworkDatabase.py
@@ -27,35 +27,21 @@ def first_split(firstBreak):
 
 def add_to_db(contentAdd):
     if len(contentAdd) == 2:
-        valID          = ''
-        val1           = "'" + contentAdd[0] + "'"
-        val2           = "'" + contentAdd[1] + "'"
-        valPages       = 0
-        today          = date.today()
-        valLink        = 'CurDir() + "ContentAdd[0]" + "ContentAdd[1]" + "PDF"'
-        valNote        = "NULL"
-        valDate        = 0
-        valThisIsABill = "NULL"
-        cont1 = cursor.execute("INSERT INTO Scanned_paperwork (ID, Type, Paperwork_name, Number_of_Pages, Date_scanned, Link_to_paperwork, Notes, Date_of_paperwork, Is_This_a_Bill)"
-                       "VALUES (valID, val1, val2, valPages, today, valLink, valNote, valDate, ValThisIsABill);"
-                       )
+        print("Name must start with originization name, then type of paperwork,\nand end with date. Must be separated with an underscore")
+
     elif len(contentAdd) == 3:
-        valID          = ''
-        val1           = contentAdd[0]
-        val2           = contentAdd[1]
-        valPages       = 0
-        today          = date.today()
-        valLink        = 'add'
-        valNote        = "NULL"
-        valDate        = contentAdd[2]
-        valThisIsABill = "NULL"
-        cursor.execute("INSERT INTO Scanned_paperwork (ID, Type, Paperwork_name, Number_of_Pages, Date_scanned, Link_to_paperwork, Notes, Date_of_paperwork, Is_This_a_Bill)"
-                       "VALUES (valID, val1, val2, valPages, today, valLink, valNote, valDate, ValThisIsABill);")
+        val1 = contentAdd[1]
+        val2 = contentAdd[0]
+        val3 = date.today()
+        valDate = val3.strftime('%m/%d/%y')
+        testVals = (val1, val2, valDate)
+        exeStatment = ("INSERT INTO Scanned_paperwork (Type, Paperwork_name, Date_scanned)"
+                "VALUES (?, ?, ?);")
+        cursor.execute(exeStatment, testVals)
+        cursor.commit()
+
     else:
         print("Error with content length")
-
-#Get Full List
-filesInFolder = copy_file_names(r"F:\ScannedRandomPaperwork\NeedToAddToDatabase")
 
 #Iterate through full list split apart and add to db
 def iter_through_full_list(filesInFolder):
@@ -63,5 +49,18 @@ def iter_through_full_list(filesInFolder):
         stripped = first_split(filesInFolder)
         print(stripped)
 
-testStripped = first_split(filesInFolder)
-testAdd = add_to_db(testStripped)
+#TODO: Create a function to compare file names in MS Access and names in folder path to weed out duplicates. Give user option to save anyway.
+def ms_file_name():
+    selectStmt = "SELECT Link_to_paperwork FROM Scanned_paperwork"
+    return selectStmt
+linksToPaperwork = ms_file_name()
+print(linksToPaperwork)
+#def search_for_duplicates(folderFiles, msFiles):
+
+#TODO: Find way to Hyperlink in SQL for link to file
+#TODO: 
+
+#Get Full List
+#filesInFolder = copy_file_names(r"F:\ScannedRandomPaperwork\NeedToAddToDatabase")
+
+#testStripped = first_split(filesInFolder)
