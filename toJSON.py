7f9b3fc4a9736692e80129de0764e656b59e464d
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-12-29 17:53:22 -0600

    Todo list created.

---
+++ toJSON.py
@@ -0,0 +1,6 @@
+#TODO: import JSON
+#TODO: Look for JSON file
+#TODO: If no JSON file create a file
+#TODO: If there is a JSON file iterate through location in file (numbered)
+#TODO: Ask user what file location is to be worked on
+
