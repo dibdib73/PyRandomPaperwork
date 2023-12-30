7832693b66a5e40cecf2b09fc20cdd9aceb6e231
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-12-29 20:39:38 -0600

    Created func look and create and class

--- toJSON.py
+++ toJSON.py
@@ -1,6 +1,21 @@
+import json
+import typing
 #TODO: import JSON
 #TODO: Look for JSON file
 #TODO: If no JSON file create a file
 #TODO: If there is a JSON file iterate through location in file (numbered)
 #TODO: Ask user what file location is to be worked on
 
+def look_for_json() -> str:
+    return None
+
+def create_json(jsonName, jsonLoc, ) -> str:
+    return None
+
+class json_read_write(self, jsonName, jsonLocation):
+    jsonName = self.jsonName
+    jsonLocation = self.jsonLocation
+    def write_to_json(self):
+        return None
+    def read_json_file(self):
+        return None
