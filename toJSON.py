e99296b3288fbff692c1c0244f24a4930152f343
dibdib73 <dan.bobloblaw.berg@gmail.com>
2023-12-30 14:19:38 -0600

    Added json to end of file name

--- toJSON.py
+++ toJSON.py
@@ -7,23 +7,32 @@ import os
 #TODO: If there is a JSON file iterate through location in file (numbered)
 #TODO: Ask user what file location is to be worked on
 
-fileN = r'Fidelity_Retirement_07012023.pdf'
+fileN = r'Fidelity_Retirement_0701202.pdf'
 #input("What is the database name? ")
 wherefile = r'F:\ScannedRandomPaperwork\NeedToAddToDatabase'
 #input("Where is the file location? ")
 
 def look_for_json(fileN: str, wherefile: str) -> str:
+    wherefile = wherefile + '.json'
     wherePath = os.listdir(wherefile)
     a = fileN in wherePath
     if a == True:
         print('Yes this file is in this dirctory.')
     else:
-        print('There is no file like this here.')
+        ans = print('This file does not exist. Do you want to create one? (y) or (n)')
+        if ans.lower() == 'y':
+            createdFile = create_json(fileN, wherefile) # If file doesn't exist create file with createfile function.
+            return createdFile
+        elif ans.lower() != 'y' or ans.lower() != 'n':
+            print('Answer must be "y" or "n" only.')
+        else:
+            print('Good bye.')
 
 def create_json(jsonName: str, jsonLoc: str ) -> str:
-    return None
+    jsonPath = os.path.join(jsonLoc, jsonName)
 
-look_for_json(fileN, wherefile)
+
+dbFile = look_for_json(fileN, wherefile)
 """class json_read_write(self, jsonName, jsonLocation):
      jsonName = self.jsonName
     jsonLocation = self.jsonLocation
